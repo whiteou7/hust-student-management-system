561d32a40e21f27dc3226aacb40d847c973aeba8
whiteou7 <fps4day@gmail.com>
2025-06-06 15:03:13 +0700

    vercel plz work

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -9,14 +9,5 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxt/eslint"],
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-05-30",
-  nitro: {
-    routeRules: {
-      "/_nuxt/**": {
-        headers: {
-          "Content-Type": "text/css"
-        }
-      }
-    }
-  },
   ssr: false
 })
